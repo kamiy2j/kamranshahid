6b96531c85db3220f2c77ec0f554056e87a52958
kamiy2j <kamiy2j@gmail.com>
2020-01-02 00:16:35 +0500

    Update jquery.twentytwenty.js

--- js/jquery.twentytwenty.js
+++ js/jquery.twentytwenty.js
@@ -11,6 +11,10 @@
       move_with_handle_only: true,
       click_to_move: false
     }, options);
+    
+      $(window).load(function() {
+      $(window).trigger("resize.twentytwenty");
+    });
 
     return this.each(function() {
 
